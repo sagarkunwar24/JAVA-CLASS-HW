0c1c09f3ea4c76e40a5846674ce930e2e03564db
Sagar Kunwar Chhetri <36431491+sagarkunwar24@users.noreply.github.com>
2019-03-08 20:47:33 -0500

    Create DriverAndDrunk

---
+++ DriverAndDrunk.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class DriverAndDrunk {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" Enter you age: ");
+		int age = sc.nextInt();
+		if(age > 21) {
+			System.out.println(" You are eligble to drive.");
+			
+		}
+		else 
+			if (age > 16) {
+				System.out.println("You are eligble to drive but not drink.");
+			}
+			else {
+				System.out.println(" You are not eligble to drink and drive.");
+			}
+	}
+
+}
