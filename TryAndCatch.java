b62defefc1d843afff039d4de840bec6b6fad7ad
Sagar Kunwar Chhetri <36431491+sagarkunwarchhetri@users.noreply.github.com>
2019-03-27 12:15:11 -0400

    Create TryAndCatch.java

---
+++ TryAndCatch.java
@@ -0,0 +1,18 @@
+public class TryAndCatch {
+
+  public static void main(String[ ] args) {
+  
+    try {
+    
+      int[] myNumbers = {1, 2, 3};
+      
+      System.out.println(myNumbers[10]);
+      
+    } catch (Exception e) {
+    
+      System.out.println("Something went wrong.");
+      
+    }
+  }
+}
+//Output: Something went wrong.
