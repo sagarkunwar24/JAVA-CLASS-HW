f8bb2fdb2b69d1f5cf8f2d154dbef780537f12bf
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-05-01 17:34:49 -0400

    Update Area1.java

--- Area1.java
+++ Area1.java
@@ -7,3 +7,15 @@ public class Area1 {
     }
 
 }
+
+
+
+public class Main {
+
+    public static void main(String args[]){
+
+        Area2 numObj = new Area2();
+
+        System.out.println("Perimeter of rectangle is "+ numObj.area(2,3));
+    }
+}
