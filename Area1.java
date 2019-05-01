75d2c63d41fb9763fa819e43a3d0e676c3cbc5bf
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-05-01 17:35:42 -0400

    Update Area1.java

--- Area1.java
+++ Area1.java
@@ -8,6 +8,18 @@ public class Area1 {
 
 }
 
+public class Area2 extends Area1 {
+
+    public double area(double a, double b){
+
+        //@Override // check sif method in base -class is overriding by method in sub class
+        double  c= 2*(a+b);
+        System.out.println("Area of rectangle : "+ super.area(a,b));
+        //super will always call from parent class (Area1)
+
+        return c;
+    }
+}
 
 
 public class Main {
