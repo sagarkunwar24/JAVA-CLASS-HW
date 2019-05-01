7f5e1f741e22a5d7c884ff8eca562e94c8710383
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-05-01 17:28:54 -0400

    Update Shape.java

--- Shape.java
+++ Shape.java
@@ -19,3 +19,13 @@ public class Rectangle extends Shape{
     }
 }
 
+public class Circle extends Shape {
+    //implements the area() of shape class
+
+    public void area(){
+        double pi = 3.14;
+        int r = 2;
+        double area = pi* r*r;//also an exmaple of method override
+        System.out.println("The area of the rectangle is: "+ area);
+    }
+}
