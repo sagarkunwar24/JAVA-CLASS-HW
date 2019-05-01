96666e68c7c8c08bd58fa4a32ecae07e18623ca2
Sagar Kunwar Chhetri <36431491+skunwarc@users.noreply.github.com>
2019-05-01 17:33:21 -0400

    Update and rename Shape.java to Interfacee.java

--- Interfacee.java
+++ Interfacee.java
@@ -1,6 +1,6 @@
 package abstract_pkg;
 
-public abstract class Shape {
+public abstract class Interfacee {
     public abstract void area();
     //this is an abstract method, it doesn;t have implementation
 }
@@ -9,7 +9,7 @@ public abstract class Shape {
 //abstarct class may have completed method itself as well as abstract mathos also
 // this is a abstract class; abstract class may or may not have abstarct method
 
-public class Rectangle extends Shape{
+public class Rectangle extends Interfacee{
     //implements the area() shape class
     public void area(){
 
@@ -19,7 +19,7 @@ public class Rectangle extends Shape{
     }
 }
 
-public class Circle extends Shape {
+public class Circle extends Interfacee {
     //implements the area() of shape class
 
     public void area(){
